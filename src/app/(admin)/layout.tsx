a079eaa55b2933ea9594c554b300bd3a5fc36242
salwazahramunir <salwazahramunir@gmail.com>
2025-10-10 09:55:44 +0700

    change ui fallback loading

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -30,7 +30,23 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen`}
       >
-        <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>
+        <Suspense
+          fallback={
+            <div className="fixed inset-0 z-[9999] bg-white bg-opacity-95 flex items-center justify-center">
+              <div className="text-center">
+                <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-gray-900 mx-auto"></div>
+                <p className="mt-4 text-lg font-medium text-gray-700">
+                  Loading...
+                </p>
+                <p className="mt-1 text-sm text-gray-500">
+                  Mohon tunggu sebentar.
+                </p>
+              </div>
+            </div>
+          }
+        >
+          {children}
+        </Suspense>
       </body>
     </html>
   );
