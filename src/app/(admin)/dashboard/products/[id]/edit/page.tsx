be6153d2bad16ca19fbe28c3750db913d011214e
salwazahramunir <salwazahramunir@gmail.com>
2025-10-23 20:54:56 +0700

    fix error

---
+++ src/app/(admin)/dashboard/products/[id]/edit/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function EditProductPage() {
+  return (
+    <div>
+      <div className="mb-6 space-y-2">
+        <h1 className="text-3xl font-playfair font-bold text-foreground">
+          Form Edit Product
+        </h1>
+        <p className="text-muted-foreground text-lg">Create New Product</p>
+      </div>
+    </div>
+  );
+}
