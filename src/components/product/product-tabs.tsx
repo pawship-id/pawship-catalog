249bb68f70bcd918ee9cc24eed41dfc48b1e4dfc
salwazahramunir <salwazahramunir@gmail.com>
2025-12-06 21:04:29 +0700

    comment size recommender

--- src/components/product/product-tabs.tsx
+++ src/components/product/product-tabs.tsx
@@ -190,7 +190,7 @@ export default function ProductTabs({ product }: ProductTabsProps) {
                   </div>
                 </div>
 
-                <div>
+                {/* <div>
                   <h3 className="font-semibold text-gray-900 mb-3">
                     Size Recommender
                   </h3>
@@ -238,7 +238,7 @@ export default function ProductTabs({ product }: ProductTabsProps) {
                       Get Size Recommendation
                     </Button>
                   </form>
-                </div>
+                </div> */}
 
                 <div className="bg-secondary border border-primary/50 rounded-lg p-4 mb-4">
                   <h3 className="font-semibold text-gray-700 mb-3">
